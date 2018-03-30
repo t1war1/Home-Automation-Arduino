d59bfa7c107f14bb2f44bfc1783c4e850820baee
Mrfunnybug <Mrfunnybug@users.noreply.github.com>
2018-03-30 20:54:10 +0530

    updating io.on function

--- server.js
+++ server.js
@@ -11,35 +11,50 @@ const board=five.Board();
 app.use(express.static(path.join(__dirname,'public_static')));
 
 
-var living_room_led, photoresistor;
+var living_room_led, photoresistor,control_pin;
 
 board.on('ready',()=>{
+   // console.log("test2")
     living_room_led=new five.Led(13);
     living_room_led.off();
+
+    control_pin=new five.Pin(12);
 
     photoresistor=new five.Sensor({
         pin: "A0",
         freq: 250
     });
-
     photoresistor.on('data',function(){
         if(this.scaleTo([0,100])<40)
         {
+        //   console.log("photoresistor on")
             living_room_led.on();
             io.emit('photoresistor-status',{
                 status:'on'
             });
         }else
         {
+          //  console.log("photoresistor off")
             living_room_led.off();
             io.emit('photoresistor-status',{
                 status:'off'
             });
         }
     })
+    photoresistor.disable();
+/*
+    control_pin.on('low',function () {
+        console.log("test1")
+        photoresistor.disable();
+    });
 
+    control_pin.on('high',function () {
+        console.log("test2")
+        photoresistor.enable();
+    })
 
 });
+*/
 
 io.on('connection',(socket)=>{
 
@@ -47,6 +62,22 @@ io.on('connection',(socket)=>{
     living_room_led.toggle();
     });
 
+    socket.on('disable-photoresistor',(data)=>{
+        
+        
+        control_pin.write(0);
+        console.log("disable photoresistor")
+        photoresistor.disable();
+
+    });
+
+    socket.on('enable-photoresistor',(data)=>{
+       
+        
+        control_pin.write(1);
+        console.log("enable photoresistor")
+        photoresistor.enable();
+    });
 
 });
 
