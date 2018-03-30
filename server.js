42892c266b3babb0d02dc742bf8849add5d711fa
Ayush Arora <vegeta1509@users.noreply.github.com>
2018-03-30 20:01:01 +0530

    changing localhost port from 3000 to 4000

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ io.on('connection',(socket)=>{
 
 });
 
-server.listen(3000,()=>{
-    console.log(`Server on at http://localhost:3000/`);
+server.listen(4000,()=>{
+    console.log(`Server on at http://localhost:4000/`);
 });
