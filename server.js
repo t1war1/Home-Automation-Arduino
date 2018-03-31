7a12b4169af8e9b67c61d8202ba6aaaa95c18810
Mrfunnybug <Mrfunnybug@users.noreply.github.com>
2018-03-31 18:11:38 +0530

    checking sensor value

--- server.js
+++ server.js
@@ -14,57 +14,59 @@ app.use(express.static(path.join(__dirname,'public_static')));
 
 var living_room_led, photoresistor,control_pin;
 
-board.on('ready',()=>{
-   
+board.on('ready',()=>
+{  
     living_room_led=new five.Led(13);
     living_room_led.off();
 
-    control_pin=new five.Pin(12);
+    control_pin=new five.Pin(12);                              //to select between button and photoresistor
 
     photoresistor=new five.Sensor({
         pin: "A0",
         freq: 250
     });
 
-    five.Pin.read(control_pin, function(error, value) {
-        // console.log(value);
+    five.Pin.read(control_pin, function(error, value) {          // reads the state of control pin
+        // console.log(value);                                   // state of control pin 
           temp = value;
       });
 
-if (temp!=0)
-{
-
-    photoresistor.on('data',function()
+    if (temp!=0)                                              // Only works when control pin will be 1 or it is selected as photoresistor
     {
-              
-     if (temp!=0)
 
+        photoresistor.on('data',function()
         {
-            if(this.scaleTo([0,100])<40)
+                
+        if (temp!=0)
+
             {
-            console.log("photoresistor on")
-                living_room_led.on();
-                io.emit('photoresistor-status',{
-                    status:'on'
-                });
-            }else
-            {
-                console.log("photoresistor off")
-                living_room_led.off();
-                io.emit('photoresistor-status',{
-                    status:'off'
-                });
-            }
-            }
+                if(this.scaleTo([0,100])<40)
+                {
+                console.log("photoresistor on")
+                    living_room_led.on();                     
+                    console.log(this.value);                       // To see the analog output value from photoresistror
+                    io.emit('photoresistor-status',{
+                        status:'on'
+                    });
+                }else
+                {
+                    console.log("photoresistor off")
+                    console.log(this.value);                        // To see the analog output value from photoresistror
+                    living_room_led.off();
+                    io.emit('photoresistor-status',{
+                        status:'off'
+                    });
+                }
+                }
             else
             {
-                
+                    
                 photoresistor.disable();
                 console.log("photoresistor disable")
             }
-    
-    })
-}
+        
+        })
+    }
 
     photoresistor.disable();
 
